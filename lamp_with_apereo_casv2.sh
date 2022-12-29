155e6d648369c433eaeccc13086e0bfa22462d86
Anoop Singh <73455995+AnupHST@users.noreply.github.com>
2022-12-29 16:39:56 +0530

    Update lamp_with_apereo_casv2.sh

--- lamp_with_apereo_casv2.sh
+++ lamp_with_apereo_casv2.sh
@@ -191,7 +191,7 @@ while true; do
             
                 case $ping1 in
                     [yY][eE][sS]|[yY]) 
-                    while true; do
+                    
                         ping=$(ping -c 3 ${DOMAIN_NAME})
                         if [[ ${ping} ]]; then
                             
@@ -203,7 +203,7 @@ while true; do
                             echo -en "$BGreen Please re-enter domain name: $BYellow" 
                             read DOMAIN_NAME  
                         fi
-                    done
+                    
                         echo  -e "$Color_Off" ;;
                     [nN][oO]|[nN])  break;;
                     
